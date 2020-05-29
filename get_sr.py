caa58c3a2d2a103d2a2b0a8defe5c49eb56d52d3
idearibosome <idearibosome@gmail.com>
2020-05-29 16:18:52 +0900

    get_sr - support chop forward option

--- get_sr.py
+++ get_sr.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 import importlib
 import json
 import os
@@ -25,6 +26,9 @@ def main():
 
   parser.add_argument('--input_path', type=str, default='LR', help='Base path of the input images.')
   parser.add_argument('--output_path', type=str, default='SR', help='Base path of the output images.')
+
+  parser.add_argument('--chop_forward', action='store_true', help='Employ chop-forward to reduce the memory usage.')
+  parser.add_argument('--chop_overlap_size', type=int, default=20, help='The overlapping size for the chop-forward process. Should be even.')
 
   args, remaining_args = parser.parse_known_args()
 
@@ -67,7 +71,14 @@ def main():
     input_image = cv.cvtColor(input_image, cv.COLOR_BGR2RGB)
     input_image = np.transpose(input_image, [2, 0, 1])
 
-    output_image = model.upscale(input_list=[input_image], scale=args.scale)[0]
+    input_split_images = _split_image(input_image, chop=args.chop_forward, overlap_size=args.chop_overlap_size)
+    output_split_images = []
+    for input_split in input_split_images:
+      output_split = model.upscale(input_list=[input_split], scale=args.scale)[0]
+      output_split_images.append(output_split)
+    
+    output_image = _combine_images(output_split_images, input_image=input_image, scale=args.scale, chop=args.chop_forward, overlap_size=args.chop_overlap_size)
+    
     output_image = np.clip(output_image, a_min=0, a_max=255)
     output_image = np.round(output_image).astype(np.uint8)
     output_image = np.transpose(output_image, [1, 2, 0])
@@ -77,6 +88,46 @@ def main():
     
   # finalize
   print('finished')
+
+
+def _split_image(image, chop, overlap_size):
+  if (not chop):
+    return [image]
+  
+  _, height, width = image.shape
+  split_height = height // 2
+  split_width = width // 2
+  half_overlap_size = overlap_size // 2
+
+  images = []
+  images.append(copy.deepcopy(image[:, :(split_height+half_overlap_size), :(split_width+half_overlap_size)]))
+  images.append(copy.deepcopy(image[:, :(split_height+half_overlap_size), (split_width-half_overlap_size):]))
+  images.append(copy.deepcopy(image[:, (split_height-half_overlap_size):, :(split_width+half_overlap_size)]))
+  images.append(copy.deepcopy(image[:, (split_height-half_overlap_size):, (split_width-half_overlap_size):]))
+  
+  return images
+
+def _combine_images(images, input_image, scale, chop, overlap_size):
+  if (len(images) == 1):
+    return images[0]
+  
+  _, height, width = input_image.shape
+  split_height = height // 2
+  split_width = width // 2
+  new_height = height * scale
+  new_width = width * scale
+  new_split_height = split_height * scale
+  new_split_width = split_width * scale
+  new_half_overlap_size = (overlap_size // 2) * scale
+
+  output_image = np.zeros([3, new_height, new_width])
+  output_image[:, :new_split_height, :new_split_width] = images[0][:, :new_split_height, :new_split_width]
+  output_image[:, :new_split_height, new_split_width:] = images[1][:, :new_split_height, new_half_overlap_size:]
+  output_image[:, new_split_height:, :new_split_width] = images[2][:, new_half_overlap_size:, :new_split_width]
+  output_image[:, new_split_height:, new_split_width:] = images[3][:, new_half_overlap_size:, new_half_overlap_size:]
+
+  return output_image
+
 
 
 if __name__ == '__main__':
