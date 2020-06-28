487a3861a2236bbb72a3729c3f78ce4e2fd68060
Geunwoo-Jeon <65698525+Geunwoo-Jeon@users.noreply.github.com>
2020-06-28 22:53:07 +0900

    Update REGO-serial.py

--- models/REGO-serial.py
+++ models/REGO-serial.py
@@ -47,7 +47,7 @@ class REGO(BaseModel):
         parser.add_argument('--num_filters', type=int, default=64, help='The number of convolutional features.')
         parser.add_argument('--len_side', type=int, default=5, help='The number of residual blocks.')
         parser.add_argument('--num_regos', type=int, default=1, help='num of serial repeat of REGO-module')
-        parser.add_argument('--weight_scale', type=float, default=1.0, help='The scaling factor.')
+        parser.add_argument('--weight_scale', type=float, default=0.1, help='The scaling factor.')
         parser.add_argument('--interpolate', type=str, default='bilinear', help='Interpolation method.')
         parser.add_argument('--learning_rate', type=float, default=1e-4, help='Initial learning rate.')
         parser.add_argument('--learning_rate_decay', type=float, default=0.5, help='Learning rate decay factor.')
