400f09c2e0392c171764b0722d9a8e41e9178974
Geunwoo-Jeon <65698525+Geunwoo-Jeon@users.noreply.github.com>
2020-07-03 18:57:52 +0900

    update kaiming_init parameter to linear

--- models/msrr_reduced_linear.py
+++ models/msrr_reduced_linear.py
@@ -24,12 +24,12 @@ def initialize_weights(net_l, scale=1):
     for net in net_l:
         for m in net.modules():
             if isinstance(m, nn.Conv2d):
-                init.kaiming_normal_(m.weight, a=0, mode='fan_in')
+                init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='linear')
                 m.weight.data *= scale  # for residual block
                 if m.bias is not None:
                     m.bias.data.zero_()
             elif isinstance(m, nn.Linear):
-                init.kaiming_normal_(m.weight, a=0, mode='fan_in')
+                init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='linear')
                 m.weight.data *= scale
                 if m.bias is not None:
                     m.bias.data.zero_()
