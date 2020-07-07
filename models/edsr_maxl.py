ccd66994921e4d98a34c8f04863b9a3500ad4c75
junhyukk <ghgh3269@gmail.com>
2020-07-07 17:00:22 +0900

    Update edsr_maxl.py

--- models/edsr_maxl.py
+++ models/edsr_maxl.py
@@ -8,15 +8,16 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 
 from models.base import BaseModel
-import pytorch_ssim
+from collections import OrderedDict
 
 
 def create_model():
-  return EDSR()
+  return EDSR_MAXL()
 
-class EDSR(BaseModel):
+class EDSR_MAXL(BaseModel):
   def __init__(self):
     super().__init__()
 
@@ -48,9 +49,14 @@ class EDSR(BaseModel):
 
     # PyTorch model
     self.model = EDSRModule(args=self.args, scale=self.scale)
+    self.label_generator = LabelGenerator(args=self.args, scale=self.scale)
     if (is_training):
       self.optim = optim.Adam(
         filter(lambda p: p.requires_grad, self.model.parameters()),
+        lr=self._get_learning_rate()
+      )
+      self.gen_optim = optim.Adam(
+        filter(lambda p: p.requires_grad, self.label_generator.parameters()),
         lr=self._get_learning_rate()
       )
       self.loss_fn = nn.L1Loss()
@@ -58,6 +64,7 @@ class EDSR(BaseModel):
     # configure device
     self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     self.model = self.model.to(self.device)
+    self.label_generator = self.label_generator.to(self.device)
       
   def save(self, base_path):
     save_path = os.path.join(base_path, 'model_%d.pth' % (self.global_step))
@@ -73,40 +80,108 @@ class EDSR(BaseModel):
     scale = self.scale_list[np.random.randint(len(self.scale_list))]
     return scale
 
-  def train_step(self, input_list, scale, truth_list, summary=None):
+  def train_step(self, input_list, scale, truth_list, summary=None, stage=2):
     # numpy to torch
     input_tensor = torch.tensor(input_list, dtype=torch.float32, device=self.device)
     truth_tensor = torch.tensor(truth_list, dtype=torch.float32, device=self.device)
+    lmbda = 0.1
+    lr_tmp = 0.01
 
-    # get SR and calculate loss
-    output_tensor = self.model(input_tensor)
-    loss = self.loss_fn(output_tensor, truth_tensor)
+    if (stage == 1):
+      # get SR and calculate loss
+      output_tensor1, output_tensor2 = self.model(input_tensor)
+      output_tensor3 = self.label_generator(input_tensor)
+      loss_pri = self.loss_fn(output_tensor1, truth_tensor)
+      loss_aux = self.loss_fn(output_tensor2, output_tensor3)
+      loss1 = loss_pri + loss_aux
 
-    # adjust learning rate
-    lr = self._get_learning_rate()
-    for param_group in self.optim.param_groups:
-      param_group['lr'] = lr
+      # tv_loss define
+      w_variation = torch.sum(torch.abs(output_tensor3[:, :, :, :-1] - output_tensor3[:, :, :, 1:]))
+      h_variation = torch.sum(torch.abs(output_tensor3[:, :, :-1, :] - output_tensor3[:, :, 1:, :]))
+      loss_tv = w_variation + h_variation
+      loss2 = loss_pri + lmbda * loss_tv 
 
-    # do back propagation
-    self.optim.zero_grad()
-    loss.backward()
-    self.optim.step()
+      # adjust learning rate
+      lr = self._get_learning_rate()
+      for param_group in self.optim.param_groups:
+        param_group['lr'] = lr
 
-    # finalize
-    self.global_step += 1
+      # do back propagation
+      self.optim.zero_grad()
+      loss1.backward()
+      self.optim.step()
 
-    # write summary
-    if (summary is not None):
-      summary.add_scalar('loss', loss, self.global_step)
-      summary.add_scalar('lr', lr, self.global_step)
+      # finalize
+      self.global_step += 1
 
-      output_tensor_uint8 = output_tensor.clamp(0, 255).byte()
-      for i in range(min(4, len(input_list))):
-        summary.add_image('input/%d' % i, input_list[i], self.global_step)
-        summary.add_image('output/%d' % i, output_tensor_uint8[i, :, :, :], self.global_step)
-        summary.add_image('truth/%d' % i, truth_list[i], self.global_step)
+      # write summary
+      if (summary is not None):
+        summary.add_scalar('loss', loss, self.global_step)
+        summary.add_scalar('lr', lr, self.global_step)
 
-    return loss.item()
+        output_tensor1_uint8 = output_tensor1.clamp(0, 255).byte()
+        for i in range(min(4, len(input_list))):
+          summary.add_image('input/%d' % i, input_list[i], self.global_step)
+          summary.add_image('output/%d' % i, output_tensor1_uint8[i, :, :, :], self.global_step)
+          summary.add_image('truth/%d' % i, truth_list[i], self.global_step)
+
+      return loss1.item()
+
+    elif (stage == 2):
+      # get SR and calculate loss
+      output_tensor1, output_tensor2 = self.model(input_tensor)
+      output_tensor3 = self.label_generator(input_tensor)
+      loss_pri = self.loss_fn(output_tensor1, truth_tensor)
+      loss_aux = self.loss_fn(output_tensor2, output_tensor3)
+      loss1 = loss_pri + loss_aux
+
+      # tv_loss define
+      w_variation = torch.sum(torch.abs(output_tensor3[:, :, :, :-1] - output_tensor3[:, :, :, 1:]))
+      h_variation = torch.sum(torch.abs(output_tensor3[:, :, :-1, :] - output_tensor3[:, :, 1:, :]))
+      loss_tv = w_variation + h_variation
+
+      # adjust learning rate
+      lr = self._get_learning_rate()
+      for param_group in self.optim.param_groups:
+        param_group['lr'] = lr
+
+      # current theta_1
+      fast_weights = OrderedDict((name, param) for (name, param) in self.model.named_parameters())
+
+      # create_graph flag for computing second-derivative
+      grads = torch.autograd.grad(loss1, self.model.parameters(), create_graph=True)
+      data = [p.data for p in list(self.model.parameters())]
+
+      # compute theta_1^+ by applying sgd on multi-task loss
+      fast_weights = OrderedDict((name, param - lr_tmp * grad) for ((name, param), grad, data) in zip(fast_weights.items(), grads, data))
+
+      # compute primary loss with the updated thetat_1^+
+      output_tensor1, output_tensor2 = self.model(input_tensor, weights=fast_weights)
+      loss_pri = self.loss_fn(output_tensor1, truth_tensor)
+
+      loss2 = loss_pri + lmbda * loss_tv 
+
+      # do back propagation
+      self.gen_optim.zero_grad()
+      loss2.backward()
+      self.gen_optim.step()
+
+      # finalize
+      self.global_step += 1
+
+      # write summary
+      if (summary is not None):
+        summary.add_scalar('loss', loss, self.global_step)
+        summary.add_scalar('lr', lr, self.global_step)
+
+        output_tensor1_uint8 = output_tensor1.clamp(0, 255).byte()
+        for i in range(min(4, len(input_list))):
+          summary.add_image('input/%d' % i, input_list[i], self.global_step)
+          summary.add_image('output/%d' % i, output_tensor1_uint8[i, :, :, :], self.global_step)
+          summary.add_image('truth/%d' % i, truth_list[i], self.global_step)
+
+      return loss2.item()
+
 
   def upscale(self, input_list, scale):
     # numpy to torch
@@ -176,7 +251,6 @@ class EDSRModule(nn.Module):
   def __init__(self, args, scale):
     super(EDSRModule, self).__init__()
 
-    self.mean_shift = MeanShift([114.4, 111.5, 103.0], sign=1.0)
     self.first_conv = nn.Conv2d(in_channels=3, out_channels=args.edsr_conv_features, kernel_size=3, stride=1, padding=1)
     
     res_block_layers = []
@@ -186,9 +260,70 @@ class EDSRModule(nn.Module):
     self.after_res_conv = nn.Conv2d(in_channels=args.edsr_conv_features, out_channels=args.edsr_conv_features, kernel_size=3, stride=1, padding=1)
 
     self.upsample = UpsampleBlock(num_channels=args.edsr_conv_features, scale=scale)
-    self.final_conv = nn.Conv2d(in_channels=args.edsr_conv_features, out_channels=3, kernel_size=3, stride=1, padding=1)
+    self.pri_conv = nn.Conv2d(in_channels=args.edsr_conv_features, out_channels=3, kernel_size=3, stride=1, padding=1)
 
-    self.mean_inverse_shift = MeanShift([114.4, 111.5, 103.0], sign=-1.0)
+    self.aux_conv = nn.Conv2d(in_channels=args.edsr_conv_features, out_channels=1, kernel_size=3, stride=1, padding=1)
+  
+  def forward(self, x, weights=None):
+    """
+        if no weights given, use the direct training strategy and update network paramters
+        else retain the computational graph which will be used in second-derivative step
+    """
+    if weights is None:
+      base = F.interpolate(x, scale_factor=4, mode='bilinear', align_corners=False)
+      x = self.first_conv(x)
+
+      res = self.res_blocks(x)
+      res = self.after_res_conv(res)
+      shared_feat = torch.add(x, res)
+
+      x = self.upsample(shared_feat)
+      x = self.pri_conv(x)
+      x_pri = torch.add(base, x)
+
+      x_aux = self.aux_conv(shared_feat)
+
+    else:
+      base = F.interpolate(x, scale_factor=4, mode='bilinear', align_corners=False)
+      x = F.conv2d(x, weights['first_conv.weight'], weights['first_conv.bias'], stride=1, padding=1)
+      before_res = x 
+
+      for i in range(16):
+        tmp = F.conv2d(x, weights['res_blocks.{:d}.body.0.weight'.format(i)], weights['res_blocks.{:d}.body.0.bias'.format(i)], stride=1, padding=1)
+        tmp = F.relu(tmp, inplace=True)
+        x += F.conv2d(tmp, weights['res_blocks.{:d}.body.2.weight'.format(i)], weights['res_blocks.{:d}.body.2.bias'.format(i)], stride=1, padding=1)
+      x = F.conv2d(x, weights['after_res_conv.weight'], weights['after_res_conv.bias'], stride=1, padding=1)
+      shared_feat = torch.add(before_res, x)
+
+      pixel_shuffle = nn.PixelShuffle(2)
+
+      x = F.conv2d(shared_feat, weights['upsample.body.0.weight'], weights['upsample.body.0.bias'], stride=1, padding=1)
+      x = pixel_shuffle(x)
+      x = F.conv2d(x, weights['upsample.body.2.weight'], weights['upsample.body.2.bias'], stride=1, padding=1)
+      x = pixel_shuffle(x)
+      x = F.conv2d(x, weights['pri_conv.weight'], weights['pri_conv.bias'], stride=1, padding=1)
+      x_pri = torch.add(base, x)
+
+      x_aux = F.conv2d(shared_feat, weights['aux_conv.weight'], weights['aux_conv.bias'], stride=1, padding=1)
+
+
+    return x_pri, x_aux
+
+class LabelGenerator(nn.Module):
+  def __init__(self, args, scale):
+    super(LabelGenerator, self).__init__()
+
+    self.mean_shift = MeanShift([114.4, 111.5, 103.0], sign=1.0)
+    self.first_conv = nn.Conv2d(in_channels=3, out_channels=args.edsr_conv_features, kernel_size=3, stride=1, padding=1)
+    
+    res_block_layers = []
+    for i in range(args.edsr_res_blocks // 4):
+      res_block_layers.append(ResidualBlock(num_channels=args.edsr_conv_features, weight=args.edsr_res_weight))
+    self.res_blocks = nn.Sequential(*res_block_layers)
+    self.after_res_conv = nn.Conv2d(in_channels=args.edsr_conv_features, out_channels=args.edsr_conv_features, kernel_size=3, stride=1, padding=1)
+
+    self.final_conv = nn.Conv2d(in_channels=args.edsr_conv_features, out_channels=1, kernel_size=3, stride=1, padding=1)
+    self.sigmoid = nn.Sigmoid()
   
   def forward(self, x):
     x = self.mean_shift(x)
@@ -198,8 +333,7 @@ class EDSRModule(nn.Module):
     res = self.after_res_conv(res)
     x = torch.add(x, res)
 
-    x = self.upsample(x)
     x = self.final_conv(x)
-    x = self.mean_inverse_shift(x)
+    x = self.sigmoid(x)
 
     return x
