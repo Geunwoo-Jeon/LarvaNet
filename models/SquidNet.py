01c5823e9863b81f8fd514f14a9bff45e159b134
Geunwoo-Jeon <65698525+Geunwoo-Jeon@users.noreply.github.com>
2020-07-11 15:06:11 +0900

    squid net added

---
+++ models/SquidNet.py
@@ -0,0 +1,278 @@
+import argparse
+import copy
+import math
+import os
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import torch.optim as optim
+import torch.nn.functional as F
+import validate
+
+from models.base import BaseModel
+
+
+def create_model():
+    return SquidNet()
+
+
+def initialize_weights(net_l, scale=1):
+    if not isinstance(net_l, list):
+        net_l = [net_l]
+    for net in net_l:
+        for m in net.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, a=0, mode='fan_in')
+                m.weight.data *= scale  # for residual block
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                init.kaiming_normal_(m.weight, a=0, mode='fan_in')
+                m.weight.data *= scale
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias.data, 0.0)
+
+
+class SquidNet(BaseModel):
+    def __init__(self):
+        super().__init__()
+
+    def parse_args(self, args):
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument('--num_modules', type=int, default=2, help='The number of residual blocks at LR domain.')
+        parser.add_argument('--num_blocks', type=int, default=16, help='The number of residual blocks at HR domain.')
+        parser.add_argument('--interpolate', type=str, default='bicubic', help='Interpolation method.')
+
+        parser.add_argument('--lr', type=float, default=1e-3, help='Initial learning rate.')
+        parser.add_argument('--lr_decay', type=float, default=0.5, help='Learning rate decay factor.')
+        parser.add_argument('--threshold', type=float, default=0.005, help='Learning rate decay factor.')
+        parser.add_argument('--min_lr', type=float, default=1e-5, help='Initial learning rate.')
+        parser.add_argument('--patience', type=int, default=10, help='patience for lr scheduler')
+        parser.add_argument('--lambda_cons', type=float, default=0.1, help='lambda for consistency loss')
+
+        self.args, remaining_args = parser.parse_known_args(args=args)
+        return copy.deepcopy(self.args), remaining_args
+
+    def prepare(self, is_training, scales, global_step=0):
+        # config. parameters
+        self.global_step = global_step
+        self.epoch = 0
+
+        self.scale_list = scales
+        for scale in self.scale_list:
+            if (not scale in [2, 3, 4]):
+                raise ValueError('Unsupported scale is provided.')
+        if len(self.scale_list) != 1:
+            raise ValueError('Only one scale should be provided.')
+        self.scale = self.scale_list[0]
+
+        # PyTorch model
+        self.modules = []
+        for i in range(self.args.num_modules):
+            if i == 0:
+                tmp_module = SquidHeadModule(args=self.args)
+                self.modules.append(tmp_module)
+            else:
+                tmp_module = SquidLegModule(args=self.args)
+                self.modules.append(nn.Sequential(self.modules[i-1], tmp_module))
+
+        legs = self.args.num_modules - 1
+        self.model = self.modules[legs]
+
+        if (is_training):
+            self.loss_fn = nn.L1Loss()
+            self.optims = []
+            for i in range(self.args.num_modules):
+                tmp_optim = optim.AdamW(
+                    filter(lambda p: p.requires_grad, self.modules[i].parameters()),
+                    lr=self.args.lr
+                )
+                self.optims.append(tmp_optim)
+            self.schedulers = []
+            for i in range(self.args.num_modules):
+                tmp_scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+                    self.optims[i], mode='max', factor=self.args.lr_decay, patience=self.args.patience,
+                    threshold=self.args.threshold, threshold_mode='abs', min_lr=self.args.min_lr, verbose=True)
+                self.schedulers.append(tmp_scheduler)
+
+        # configure device
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model = self.model.to(self.device)
+
+    def save(self, base_path):
+        save_path = os.path.join(base_path, 'model_%depoch.pth' % (self.epoch))
+        torch.save(self.model.state_dict(), save_path)
+
+    def restore(self, ckpt_path, target=None):
+        self.model.load_state_dict(torch.load(ckpt_path, map_location=self.device))
+  
+    def get_model(self):
+        return self.model
+
+    def get_next_train_scale(self):
+        scale = self.scale_list[np.random.randint(len(self.scale_list))]
+        return scale
+
+    def train_step_squid(self, args, val_dataloader, input_list, scale, truth_list, summary=None):
+        self.global_step += 1
+
+        # numpy to torch
+        input_tensor = torch.as_tensor(input_list, dtype=torch.float32, device=self.device)
+        truth_tensor = torch.as_tensor(truth_list, dtype=torch.float32, device=self.device)
+
+        # train each legs
+        for i in range(self.args.num_modules):
+            if self.global_step < i * self.epoch:
+                continue
+            # get SR and calculate loss
+            output_tensor = self.modules[i](input_tensor)
+            loss = self.loss_fn(output_tensor, truth_tensor)
+            # do back propagation
+            self.optims[i].zero_grad()
+            loss.backward()
+            self.optims[i].step()
+            if self.global_step % args.step_per_epoch == 0:
+                self.validate_for_train(args, val_dataloader, self.schedulers[i])
+
+        # write summary
+        lr = self.get_lr()
+        if summary is not None:
+            summary.add_scalar('loss', loss, model.global_step)
+            summary.add_scalar('lr', lr, model.global_step)
+
+            input_tensor_uint8 = input_tensor.clamp(0, 255).byte()
+            output_tensor_uint8 = output_tensor.clamp(0, 255).byte()
+            truth_tensor_uint8 = truth_tensor.clamp(0, 255).byte()
+            for i in range(min(4, len(input_list))):
+                summary.add_image('input/%d' % i, input_tensor_uint8[i, :, :, :], self.epoch)
+                summary.add_image('output/%d' % i, output_tensor_uint8[i, :, :, :], self.epoch)
+                summary.add_image('truth/%d' % i, truth_tensor_uint8[i, :, :, :], self.epoch)
+
+        return loss.item()
+
+    def validate_for_train(self, args, dataloader, scheduler):
+        # scheduling lr by validation
+        if self.global_step % (args.step_per_epoch * 10) == 0:
+            self.epoch += 1
+            print('begin validation')
+            num_images = dataloader.get_num_images()
+            psnr_list = []
+
+            for image_index in range(num_images):
+                input_image, truth_image, image_name = dataloader.get_image_pair(image_index=image_index,
+                                                                                     scale=4)
+                output_image = self.upscale(input_list=[input_image], scale=4)[0]
+                truth_image = validate._image_to_uint8(truth_image)
+                output_image = validate._image_to_uint8(output_image)
+                truth_image = validate._fit_truth_image_size(output_image=output_image, truth_image=truth_image)
+
+                psnr = validate._image_psnr(output_image=output_image, truth_image=truth_image)
+                psnr_list.append(psnr)
+
+            average_psnr = np.mean(psnr_list)
+            print(f'step {self.global_step}, epoch {self.global_step / args.step_per_epoch:.0f},'
+                  f' psnr={average_psnr:.8f}, lr = {self.get_lr():.6f}')
+
+            scheduler.step(average_psnr)
+
+            # save model after 10 epochs
+            if self.epoch % 10 == 0:
+                self.save(base_path=args.train_path)
+                print('saved a model checkpoint at epoch %d' % self.epoch)
+
+    def upscale(self, input_list, scale):
+        # numpy to torch
+        input_tensor = torch.tensor(input_list, dtype=torch.float32, device=self.device)
+
+        # get SR
+        output_tensor = self.model(input_tensor)
+
+        # finalize
+        return output_tensor.detach().cpu().numpy()
+
+    def get_lr(self):
+        return self.optims[0].param_groups[0]['lr']
+
+    def fwd_runtime(self, input_tensor):
+        output_tensor = self.model(input_tensor)
+        return output_tensor
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, num_channels):
+        super(ResidualBlock, self).__init__()
+
+        self.body = nn.Sequential(
+            nn.Conv2d(in_channels=num_channels, out_channels=num_channels, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=num_channels, out_channels=num_channels, kernel_size=3, stride=1, padding=1)
+        )
+        # initialization
+        initialize_weights(self.body, 0.1)
+
+    def forward(self, x):
+        res = self.body(x)
+        output = torch.add(x, res)
+        return output
+
+
+class SquidHeadModule(nn.Module):
+    def __init__(self, args):
+        super(SquidHeadModule, self).__init__()
+        num_filters = 48
+        self.first_conv = nn.Conv2d(in_channels=3, out_channels=num_filters, kernel_size=3, stride=1,
+                                    padding=1)
+        res_block_layers = []
+        for i in range(args.num_blocks):
+            res_block_layers.append(ResidualBlock(num_channels=num_filters))
+        self.res_blocks = nn.Sequential(*res_block_layers)
+        self.upsample = nn.PixelShuffle(4)
+
+        # activation function
+        self.lrelu = nn.LeakyReLU(negative_slope=0.1, inplace=True)
+
+        # initialization
+        initialize_weights(self.first_conv, 0.1)
+
+        self.interpolate = args.interpolate
+
+    def forward(self, x):
+        out = self.lrelu(self.first_conv(x))
+        out = self.res_blocks(out)
+        out = self.upsample(out)
+        base = F.interpolate(x, scale_factor=4, mode=self.interpolate, align_corners=False)
+        out += base
+        return out
+
+
+class SquidLegModule(nn.Module):
+    def __init__(self, args):
+        super(SquidLegModule, self).__init__()
+        num_filters = 48
+        res_block_layers = []
+        for i in range(args.num_blocks):
+            res_block_layers.append(ResidualBlock(num_channels=num_filters))
+        self.res_blocks = nn.Sequential(*res_block_layers)
+        self.upsample = nn.PixelShuffle(4)
+
+        # activation function
+        self.lrelu = nn.LeakyReLU(negative_slope=0.1, inplace=True)
+
+    def pixel_unshuffle(self, x, scale):
+        n, c, h, w = x.size()
+        unfolded_x = torch.nn.functional.unfold(x, scale, stride=scale)
+        return unfolded_x.view(n, c * scale ** 2, h // scale, w // scale)
+
+    def forward(self, x):
+        fea = self.pixel_unshuffle(x, 4)
+        out = self.res_blocks(self.lrelu(fea))
+        out = self.upsample(out)
+        out += x
+        return out
--- train_squid.py
+++ train_squid.py
@@ -6,10 +6,8 @@ import time
 
 import dataloaders
 import models
-import validate
 
 import torch
-import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 from math import log10, floor
 
@@ -39,7 +37,7 @@ def main():
     parser.add_argument('--max_steps', type=int, default=300000, help='The maximum number of training steps.')
     parser.add_argument('--log_freq', type=int, default=10, help='The frequency of logging.')
     parser.add_argument('--summary_freq', type=int, default=1000, help='The frequency of logging on TensorBoard.')
-    parser.add_argument('--save_freq', type=int, default=1000, help='The frequency of saving the trained model.')
+    parser.add_argument('--save_freq', type=int, default=10000, help='The frequency of saving the trained model.')
     parser.add_argument('--sleep_ratio', type=float, default=0.05,
                         help='The ratio of sleeping time for each training step, which prevents overheating of GPUs. Specify 0 to disable sleeping.')
 
@@ -48,7 +46,6 @@ def main():
     parser.add_argument('--restore_target', type=str, help='Target of the restoration.')
     parser.add_argument('--global_step', type=int, default=0,
                         help='Initial global step. Specify this to resume the training.')
-
 
     args, remaining_args = parser.parse_known_args()
 
@@ -64,8 +61,8 @@ def main():
     dataloader.prepare(scales=scale_list)
 
     DATALOADER_MODULE = importlib.import_module('dataloaders.' + args.dataloader_val)
-    dataloader_val = DATALOADER_MODULE.create_loader()
-    dataloader_val.prepare(scales=scale_list)
+    val_dataloader = DATALOADER_MODULE.create_loader()
+    val_dataloader.prepare(scales=scale_list)
 
     # model
     print('prepare model - %s' % (args.model))
@@ -104,6 +101,7 @@ def main():
         batch_data_size = (args.input_patch_size**2)*args.batch_size*3
         dataset_size = 300*(1024**2)
         step_per_epoch = round_to_1(dataset_size/batch_data_size)
+        args.step_per_epoch = step_per_epoch
     else:
         step_per_epoch = args.step_per_epoch
 
@@ -114,86 +112,22 @@ def main():
         while True:
             model.global_step += 1
 
+            start_time = time.time()
+
             scale = model.get_next_train_scale()
             summary = summary_writers[scale] if (model.global_step % args.summary_freq == 0) else None
+            input_list, truth_list = dataloader.get_patch_batch(batch_size=args.batch_size, scale=scale,
+                                                                input_patch_size=args.input_patch_size)
+            loss = model.train_step_squid(args=args, val_dataloader=val_dataloader,
+                                          input_list=input_list, scale=scale, truth_list=truth_list, summary=summary)
 
-            start_time = time.time()
-            if (dataloader.is_threaded):
-                input_list, truth_list = dataloader.get_queue_data(scale=scale)
-            else:
-                input_list, truth_list = dataloader.get_patch_batch(batch_size=args.batch_size, scale=scale,
-                                                                    input_patch_size=args.input_patch_size)
-            dataload_time = time.time() - start_time
+            duration = time.time() - start_time
 
-            # numpy to torch
-            check_time = time.time()
-            input_tensor = torch.as_tensor(input_list, dtype=torch.float32, device=model.device)
-            truth_tensor = torch.as_tensor(truth_list, dtype=torch.float32, device=model.device)
-            np2ts_time = time.time() - check_time
-
-            # get SR and calculate loss
-            check_time = time.time()
-            output_tensor = model.model(input_tensor)
-            loss = model.loss_fn(output_tensor, truth_tensor)
-            fwd_loss_time = time.time() - check_time
-
-            # do back propagation
-            check_time = time.time()
-            model.optim.zero_grad()
-            loss.backward()
-            model.optim.step()
-            back_prop_time = time.time() - check_time
-
-            # scheduling lr by validation
-            if model.global_step % (10*step_per_epoch) == 0:
-                print('begin validation')
-                num_images = dataloader_val.get_num_images()
-                psnr_list = []
-
-                for image_index in range(num_images):
-                    input_image, truth_image, image_name = dataloader_val.get_image_pair(image_index=image_index,
-                                                                                        scale=scale)
-                    output_image = model.upscale(input_list=[input_image], scale=scale)[0]
-                    truth_image = validate._image_to_uint8(truth_image)
-                    output_image = validate._image_to_uint8(output_image)
-                    truth_image = validate._fit_truth_image_size(output_image=output_image, truth_image=truth_image)
-
-                    psnr = validate._image_psnr(output_image=output_image, truth_image=truth_image)
-                    psnr_list.append(psnr)
-
-                average_psnr = np.mean(psnr_list)
-                print(f'step {model.global_step}, epoch {model.global_step/step_per_epoch:.0f},'
-                      f' psnr={average_psnr:.8f}, lr = {model.get_lr():.10f}')
-
-                if hasattr(model, 'lr_scheduler'):
-                    model.lr_scheduler.step(average_psnr)
-
-                # save model after each validation
-                model.save(base_path=args.train_path)
-                print('saved a model checkpoint at step %d' % model.global_step)
-
-            # write summary
-            lr = model.get_lr()
-            if summary is not None:
-                summary.add_scalar('loss', loss, model.global_step)
-                summary.add_scalar('lr', lr, model.global_step)
-
-                input_tensor_uint8 = input_tensor.clamp(0, 255).byte()
-                output_tensor_uint8 = output_tensor.clamp(0, 255).byte()
-                truth_tensor_uint8 = truth_tensor.clamp(0, 255).byte()
-                for i in range(min(4, len(input_list))):
-                    summary.add_image('input/%d' % i, input_tensor_uint8[i, :, :, :], model.global_step)
-                    summary.add_image('output/%d' % i, output_tensor_uint8[i, :, :, :], model.global_step)
-                    summary.add_image('truth/%d' % i, truth_tensor_uint8[i, :, :, :], model.global_step)
-
-            duration =time.time() - start_time
             if args.sleep_ratio > 0 and duration > 0:
                 time.sleep(min(10.0, duration * args.sleep_ratio))
-
-            if model.global_step < step_per_epoch and model.global_step % args.log_freq == 0:
+            lr = model.get_lr()
+            if model.global_step < step_per_epoch * 2 and model.global_step % args.log_freq == 0:
                 print('step %d, lr %.10f, loss %.6f (%.3f sec/batch)' % (model.global_step, lr, loss, duration))
-                print(f'dataload_time:{dataload_time:.4f}s, np2ts_time:{np2ts_time:.4f}s, '
-                    f'fwd_loss_time: {fwd_loss_time:.4f}s, back_prop_time: {back_prop_time}s')
     except KeyboardInterrupt:
         print('interrupted (KeyboardInterrupt)')
     except Exception as e:
@@ -203,7 +137,7 @@ def main():
     print('finished')
     for scale in scale_list:
         summary_writers[scale].close()
-    if (dataloader.is_threaded):
+    if dataloader.is_threaded:
         dataloader.stop_queue_runners()
 
 
